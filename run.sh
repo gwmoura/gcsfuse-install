8dcc87a0d987faf972f995c9d2c0febcb0024aba
George Moura <gwmoura@gmail.com>
2015-12-31 12:15:14 -0200

    check if string exist in bash script

--- run.sh
+++ run.sh
@@ -13,8 +13,6 @@ if [ -z $FOLDER_TO_MOUNT ]; then
 fi
 
 export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
-export GOOGLE_APPLICATION_CREDENTIALS="/home/$USER/.gcloud/credentials.json"
-echo "export GOOGLE_APPLICATION_CREDENTIALS=\"/home/$USER/.gcloud/credentials.json\"" >> ~/.bashrc
 echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update
@@ -22,5 +20,19 @@ sudo apt-get install -y gcsfuse daemon
 sudo usermod -a -G fuse $USER
 mkdir -p $FOLDER_TO_MOUNT
 mkdir -p /home/$USER/.gcloud
-echo "daemon -- gcsfuse --implicit-dirs $BUCKET_NAME $FOLDER_TO_MOUNT" >> ~/.bashrc
+
+command="export GOOGLE_APPLICATION_CREDENTIALS=\"/home/$USER/.gcloud/credentials.json\""
+if grep -q "$command" "/home/$USER/.bashrc"; then
+ echo "This line ( $command ) exists in ~/.bashrc"
+else
+ echo "$command" >> ~/.bashrc
+fi
+
+command="daemon -- gcsfuse --implicit-dirs $BUCKET_NAME $FOLDER_TO_MOUNT"
+if grep -q "$command" "/home/$USER/.bashrc"; then
+ echo "This line ( $command ) exists in ~/.bashrc"
+else
+ echo "$command" >> ~/.bashrc 
+fi
+
 echo "Put your Google Cloud credentials account's JSON key file in /home/$USER/.gcloud/ with name credentials.json and run 'exec -l $SHELL' to restart the shell"
