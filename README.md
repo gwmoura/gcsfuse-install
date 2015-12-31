a3bfd2b665a1f1399bd700cf00a85665fea41117
George Moura <gwmoura@gmail.com>
2015-12-31 11:41:52 -0200

    update README.md and modify run.sh

--- README.md
+++ README.md
@@ -6,14 +6,14 @@ Is a bash script to install and configure gcsfuse in ubuntu machines
 
 1 - In your machine shell or terminal run:
 
-`curl -sL https://raw.githubusercontent.com/lememilitar/gcsfuse-install/master/run.bash | bash [bucket_name]` - 
+`curl -sL https://raw.githubusercontent.com/lememilitar/gcsfuse-install/master/run.sh | bash -s [bucket_name]` - 
 by default we use the folder "/home/$USER/cloudstorage" to mount your bucket
 
 or if you want to set the folder:
 
-`curl -sL https://raw.githubusercontent.com/lememilitar/gcsfuse-install/master/run.bash | bash [bucket_name] [folder_to_mount]`
+`curl -sL https://raw.githubusercontent.com/lememilitar/gcsfuse-install/master/run.sh | bash -s [bucket_name] [folder_to_mount]`
 
-2 - Put the json file credentials in ~/.gcloud/ 
+2 - Put the json file credentials in ~/.gcloud/ with name credentials.json 
 
 3 - Restart your shell
 
--- run.sh
+++ run.sh
@@ -9,12 +9,12 @@ if [ -z $BUCKET_NAME ]; then
 fi
 
 if [ -z $FOLDER_TO_MOUNT ]; then
- $FOLDER_TO_MOUNT="/home/$USER/cloudstorage"
+ FOLDER_TO_MOUNT="/home/$USER/cloudstorage"
 fi
 
 export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
-export GOOGLE_APPLICATION_CREDENTIALS="/home/$USER/.gcloud/*.json"
-echo "export GOOGLE_APPLICATION_CREDENTIALS=\"/home/$USER/.gcloud/*.json\"" >> ~/.bashrc
+export GOOGLE_APPLICATION_CREDENTIALS="/home/$USER/.gcloud/credentials.json"
+echo "export GOOGLE_APPLICATION_CREDENTIALS=\"/home/$USER/.gcloud/credentials.json\"" >> ~/.bashrc
 echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update
@@ -23,4 +23,4 @@ sudo usermod -a -G fuse $USER
 mkdir -p $FOLDER_TO_MOUNT
 mkdir -p /home/$USER/.gcloud
 echo "daemon -- gcsfuse --implicit-dirs $BUCKET_NAME $FOLDER_TO_MOUNT" >> ~/.bashrc
-echo "Put your Google Cloud credentials account's JSON key file in /home/$USER/.gcloud/ and run 'exec -l $SHELL' to restart the shell"
+echo "Put your Google Cloud credentials account's JSON key file in /home/$USER/.gcloud/ with name credentials.json and run 'exec -l $SHELL' to restart the shell"
